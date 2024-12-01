d8bc4528ad3b9ab0e4bdfe6c860ed310bf48385f
hasanasadov <hasanaliasadov@gmail.com>
2024-12-01 23:42:42 +0400

    deploy

--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -7,7 +7,7 @@ const router = Router();
 router.post("/login", authenticate, authController.login);
 router.post("/register", authController.register);
 router.post("/logout", authController.logout);
-router.get("/current-user", authorize(), authController.currentUser);
+router.get("/current-user", authController.currentUser);
 router.post("/forgot-password", authController.forgotPassword)
 router.post("/reset-password", authController.resetPassword);
 
